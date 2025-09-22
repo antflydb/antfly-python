3fce3642555e2026eb7f837e9a776c6589024fce
AJ Roetker <ajroetker@antfly.io>
2025-09-22 11:27:57 -0700

    Remove unused antfly types and add type for index stats

--- src/antfly/client_generated/models/antfly_type.py
+++ src/antfly/client_generated/models/antfly_type.py
@@ -3,22 +3,16 @@ from enum import Enum
 
 class AntflyType(str, Enum):
     BLOB = "blob"
-    BOOL = "bool"
     BOOLEAN = "boolean"
     DATETIME = "datetime"
     EMBEDDING = "embedding"
-    FLOAT = "float"
     GEOPOINT = "geopoint"
     GEOSHAPE = "geoshape"
-    INT = "int"
     KEYWORD = "keyword"
     LINK = "link"
     NUMERIC = "numeric"
     SEARCH_AS_YOU_TYPE = "search_as_you_type"
-    STRING = "string"
     TEXT = "text"
-    TIME = "time"
-    UINT = "uint"
 
     def __str__(self) -> str:
         return str(self.value)
